5ac8ab4883f31c3a18f540e86ce5ddc370186914
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-12 16:18:02 -0300

    fix: increase vm disk size

--- core/virtualisation.nix
+++ core/virtualisation.nix
@@ -4,6 +4,7 @@
   virtualisation.vmVariant = {
     virtualisation.memorySize = 4096;
     virtualisation.cores = 4;
+    virtualisation.diskSize = 16384; # MiB — default 1024 is too small for desktop + HM
 
     home-manager.extraSpecialArgs.monitors = {
       primary = {
