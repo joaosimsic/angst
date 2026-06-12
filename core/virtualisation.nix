0f87da002209fa99b82e3aef1b6b7518966c8f76
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-12 14:40:20 -0300

    feat: default grab on hover vm

--- core/virtualisation.nix
+++ core/virtualisation.nix
@@ -18,7 +18,7 @@
 
     virtualisation.qemu.options = [
       "-device virtio-vga,xres=1920,yres=1080"
-      "-display gtk,zoom-to-fit=on"
+      "-display gtk,zoom-to-fit=on,grab-on-hover=on"
       "-chardev qemu-vdagent,id=ch1,name=vdagent,clipboard=on"
       "-device virtio-serial-pci"
       "-device virtserialport,chardev=ch1,id=ch1,name=com.redhat.spice.0"
