ba02c05fb054f14c45ce281df9eb8cdc31be6b19
joaosimsic <jpsimsic@hotmail.com>
2026-06-13 23:10:37 -0300

    refac: dynamic capabilities

--- capabilities/default.nix
+++ capabilities/default.nix
@@ -1,11 +1,19 @@
 { ... }:
 
-{
-  audio = ./audio.nix;
-  container = ./container.nix;
-  git = ./git.nix;
-  graphical = ./graphical.nix;
-  network = ./network.nix;
-  search = ./search.nix;
-  ssh = ./ssh.nix;
-}
+let
+  entries = builtins.readDir ./.;
+  names = builtins.attrNames entries;
+
+  isNixFile = name:
+    let len = builtins.stringLength name;
+    in len > 4
+       && builtins.substring (len - 4) 4 name == ".nix"
+       && name != "default.nix"
+       && entries.${name} == "regular";
+
+  toAttr = name: {
+    name = builtins.substring 0 (builtins.stringLength name - 4) name;
+    value = ./${name};
+  };
+in
+builtins.listToAttrs (map toAttr (builtins.filter isNixFile names))
