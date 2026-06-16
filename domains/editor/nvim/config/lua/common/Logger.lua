1cbf33954b96cc39d406ad6e33ed098593478bf7
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-16 14:48:40 -0300

    refac: better logger

--- domains/editor/nvim/config/lua/common/Logger.lua
+++ domains/editor/nvim/config/lua/common/Logger.lua
@@ -3,35 +3,41 @@
 local Logger = {}
 Logger.__index = Logger
 
----@alias Level
----| "debug"
----| "info"
----| "warn"
----| "error"
+---@alias Level "debug" | "info" | "warn" | "error"
 
----@type table<Level, integer>
 local level_map = {
-	debug = vim.log.levels.DEBUG,
-	info = vim.log.levels.INFO,
-	warn = vim.log.levels.WARN,
-	error = vim.log.levels.ERROR,
+	debug = { vim.log.levels.DEBUG, 1 },
+	info = { vim.log.levels.INFO, 2 },
+	warn = { vim.log.levels.WARN, 3 },
+	error = { vim.log.levels.ERROR, 4 },
 }
+
+local GLOBAL_THRESHOLD = "warn"
 
 ---@param tag string
 ---@return Logger
 function Logger.new(tag)
 	---@type Logger
 	local self = setmetatable({}, Logger)
-
 	self.tag = tag
-
 	return self
 end
 
 ---@param level Level
 ---@param msg string
 function Logger:log(level, msg)
-	vim.notify(string.format("[%s] %s", self.tag, msg), level_map[level])
+	local current_score = level_map[level][2]
+	local threshold_score = level_map[GLOBAL_THRESHOLD][2]
+
+	if current_score < threshold_score then
+		return
+	end
+
+	local text = string.format("[%s] %s: %s", self.tag, level:upper(), msg)
+
+	vim.schedule(function()
+		vim.notify(text, level_map[level][1])
+	end)
 end
 
 return Logger
