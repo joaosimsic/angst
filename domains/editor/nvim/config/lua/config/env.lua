4290b236f0de0fcc5412efa962c53be52fd69763
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-16 17:09:32 -0300

    feat: config env

--- domains/editor/nvim/config/lua/config/env.lua
+++ domains/editor/nvim/config/lua/config/env.lua
@@ -2,3 +2,30 @@ vim.opt.runtimepath:prepend(vim.fn.stdpath("config") .. "/parser")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.opt.equalalways = false
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.termguicolors = true
+
+vim.opt.cursorline = true
+
+vim.opt.colorcolumn = "100,101"
+
+vim.o.guicursor = "n-v-c:block"
+
+vim.diagnostic.config({
+	virtual_text = true,
+	severity_sort = true,
+	float = { border = "none" },
+	update_in_insert = false,
+})
--- hosts/personal/default.nix
+++ hosts/personal/default.nix
@@ -10,8 +10,8 @@
     ssh = {
       identityFile = "~/.ssh/id_ed25519";
       authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINOsmeoPJnZEE7AnCpSik4QsgjLr3cRy8W3Nmi0Ee5OF jpsimsic@hotmail.com"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEeJVdPiDa6ato/McVlwbjwifFmKoqF0/sPVCLSo82kQ joao.simsic@4x4brasil.com.br"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPSgA6EHXcywZBbJuJnULFMz4Did28tsR1Rtg7dHCBAB jpsimsic@hotmail.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINn72OmW1reZqSV3UIRiYEOr6yFLYfwl/XmrscGFQwKz joao.simsic@4x4brasil.com.br"
       ];
     };
 
