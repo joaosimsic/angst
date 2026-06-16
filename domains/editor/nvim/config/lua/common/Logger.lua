5d48ff73bc710d94b62f753df37f62099371873d
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-16 15:13:41 -0300

    feat: lazy logging

--- domains/editor/nvim/config/lua/common/Logger.lua
+++ domains/editor/nvim/config/lua/common/Logger.lua
@@ -4,6 +4,7 @@ local Logger = {}
 Logger.__index = Logger
 
 ---@alias Level "debug" | "info" | "warn" | "error"
+---@alias LogMessage string|fun():string
 
 local level_map = {
 	debug = { vim.log.levels.DEBUG, 1 },
@@ -24,20 +25,44 @@ function Logger.new(tag)
 end
 
 ---@param level Level
----@param msg string
+---@param msg LogMessage
 function Logger:log(level, msg)
-	local current_score = level_map[level][2]
-	local threshold_score = level_map[GLOBAL_THRESHOLD][2]
+	local current = level_map[level]
+	local threshold = level_map[GLOBAL_THRESHOLD]
 
-	if current_score < threshold_score then
+	if not current or current[2] < threshold[2] then
 		return
+	end
+
+	if type(msg) == "function" then
+		msg = msg()
 	end
 
 	local text = string.format("[%s] %s: %s", self.tag, level:upper(), msg)
 
 	vim.schedule(function()
-		vim.notify(text, level_map[level][1])
+		vim.notify(text, current[1])
 	end)
+end
+
+---@param msg LogMessage
+function Logger:debug(msg)
+	self:log("debug", msg)
+end
+
+---@param msg LogMessage
+function Logger:info(msg)
+	self:log("info", msg)
+end
+
+---@param msg LogMessage
+function Logger:warn(msg)
+	self:log("warn", msg)
+end
+
+---@param msg LogMessage
+function Logger:error(msg)
+	self:log("error", msg)
 end
 
 return Logger
