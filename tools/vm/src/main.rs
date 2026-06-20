0a7a1c303016128c1bf8af7b72ec9ebea948c329
joaosimsic <jpsimsic@hotmail.com>
2026-06-20 01:05:37 -0300

    feat: main

--- tools/vm/src/main.rs
+++ tools/vm/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use vm_cli::run_cli;
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = run_cli().await {
+        println!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
