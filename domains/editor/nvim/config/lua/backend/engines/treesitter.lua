b176891485af1cc83f3d63ca7ffd4757d67ecc23
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-22 12:13:27 -0300

    refac: treesitter

--- domains/editor/nvim/config/lua/backend/engines/treesitter.lua
+++ domains/editor/nvim/config/lua/backend/engines/treesitter.lua
@@ -1,19 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
+  "treesitter",
+  virtual = true,
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		vim.opt.runtimepath:prepend(vim.fn.expand("~/.local/share/tree-sitter"))
 
-		require("nvim-treesitter").setup({
-			sync_install = false,
-			auto_install = false,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
+		local grammar_mappings = {
+			cs = "c_sharp",
+			razor = "c_sharp",
+			sh = "bash",
+			bash = "bash",
+		}
+
+		for filetype, grammar in pairs(grammar_mappings) do
+			vim.treesitter.language.add(filetype, { lang = grammar })
+		end
+
+		vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+			pattern = "*",
+			callback = function()
+				local ok, _ = pcall(vim.treesitter.start)
+
+				if ok then
+					vim.bo.indentexpr = "v:lua.vim.treesitter.foldexpr()"
+				end
+			end,
 		})
 	end,
 }
