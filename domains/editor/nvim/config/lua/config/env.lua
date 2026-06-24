e088372745c5dad099db5ffbccce037d9329ca44
joaosimsic <jpsimsic@hotmail.com>
2026-06-24 10:16:33 -0300

    refac: env

--- domains/editor/nvim/config/lua/config/env.lua
+++ domains/editor/nvim/config/lua/config/env.lua
@@ -1,19 +1,21 @@
-vim.opt.equalalways = false
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.relativenumber = true
-vim.opt.number = true
-
-vim.opt.termguicolors = true
-
-vim.opt.cursorline = true
-
-vim.opt.colorcolumn = "100,101"
-
-vim.o.guicursor = "n-v-c:block"
-
-vim.opt.showmode = false
+---@type Plugin
+return {
+	"env",
+	virtual = true,
+	lazy = false,
+	priority = 1001,
+	config = function()
+		vim.opt.equalalways = false
+		vim.opt.tabstop = 4
+		vim.opt.softtabstop = 4
+		vim.opt.shiftwidth = 4
+		vim.opt.expandtab = true
+		vim.opt.relativenumber = true
+		vim.opt.number = true
+		vim.opt.termguicolors = true
+		vim.opt.cursorline = true
+		vim.opt.colorcolumn = "100,101"
+		vim.o.guicursor = "n-v-c:block"
+		vim.opt.showmode = false
+	end,
+}
