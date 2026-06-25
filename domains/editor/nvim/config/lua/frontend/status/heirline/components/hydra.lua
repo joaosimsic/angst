4c576cd6ffe9efd049761690a372bde81f46ba88
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-25 17:13:23 -0300

    fix: heirline not updating

--- domains/editor/nvim/config/lua/frontend/status/heirline/components/hydra.lua
+++ domains/editor/nvim/config/lua/frontend/status/heirline/components/hydra.lua
@@ -3,13 +3,34 @@ local utils = require("frontend.status.heirline.utils")
 ---@type HeirlineComponent
 local HydraComponent = {
 	condition = function()
-		return vim.g.active_hydra ~= nil
+		if vim.g.active_hydra == nil then
+			return false
+		end
+
+		local current_buf = vim.api.nvim_get_current_buf()
+		local keymaps = vim.api.nvim_buf_get_keymap(current_buf, "n")
+
+		local has_hydra_mappings = false
+		for _, map in ipairs(keymaps) do
+			if map.desc and (map.desc:find("Hydra") or map.desc:find("DOKTOR")) then
+				has_hydra_mappings = true
+				break
+			end
+		end
+
+		return has_hydra_mappings
 	end,
 
 	init = function(self)
 		---@type ActiveHydraState
 		self.hydra = vim.g.active_hydra
 	end,
+
+	update = {
+		"ModeChanged",
+		"BufEnter",
+		"WinEnter",
+	},
 
 	{
 		provider = function(self)
