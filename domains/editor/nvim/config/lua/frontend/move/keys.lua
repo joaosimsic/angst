e61e1cfbbeb65c366a9a41aa2fe254d3ceba3cca
joaosimsic <jpsimsic@hotmail.com>
2026-06-27 00:43:16 -0300

    fix: move messages

--- domains/editor/nvim/config/lua/frontend/move/keys.lua
+++ domains/editor/nvim/config/lua/frontend/move/keys.lua
@@ -8,19 +8,19 @@ M.setup = function()
 
 	binder:map({ "x" }, "<A-j>", function()
 		move.move_text_vertical("j")
-	end, { desc = "Move text down" })
+	end, { desc = "Move text down", silent = true })
 
 	binder:map({ "x" }, "<A-k>", function()
 		move.move_text_vertical("k")
-	end, { desc = "Move text up" })
+	end, { desc = "Move text up", silent = true })
 
 	binder:map({ "x" }, "<A-h>", function()
 		move.move_text_horizontal("h")
-	end, { desc = "Move text to the left" })
+	end, { desc = "Move text to the left", silent = true })
 
 	binder:map({ "x" }, "<A-l>", function()
 		move.move_text_horizontal("l")
-	end, { desc = "Move text to the right" })
+	end, { desc = "Move text to the right", silent = true })
 end
 
 return M
--- domains/editor/nvim/config/lua/frontend/move/move.lua
+++ domains/editor/nvim/config/lua/frontend/move/move.lua
@@ -2,7 +2,7 @@ local M = {}
 
 ---@param direction "j"|"k"
 M.move_text_vertical = function(direction)
-	vim.cmd([[:execute "normal! \<ESC>"]])
+	vim.cmd([[:execute "silent! normal! \<ESC>"]])
 
 	if direction == "j" then
 		vim.cmd("silent! '<,'>move '>+1")
@@ -10,11 +10,21 @@ M.move_text_vertical = function(direction)
 		vim.cmd("silent! '<,'>move '<-2")
 	end
 
-	vim.cmd("normal! gv")
+	vim.cmd("silent! normal! gv")
 end
 
 ---@param direction "h"|"l"
 M.move_text_horizontal = function(direction)
+	local sw = vim.o.shiftwidth
+
+	if sw == 0 then
+		sw = vim.bo.tabstop
+	end
+
+	if sw == 0 then
+		sw = 4
+	end
+
 	if direction == "h" then
 		local start_line = vim.fn.line("'<")
 		local end_line = vim.fn.line("'>")
@@ -35,7 +45,7 @@ M.move_text_horizontal = function(direction)
 	end
 
 	local action = (direction == "h") and "<" or ">"
-	vim.cmd(string.format("normal! %sgv", action))
+	vim.cmd(string.format("silent! normal! %sgv", action))
 end
 
 return M
