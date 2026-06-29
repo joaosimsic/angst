e0914a4ca2eb6f6b6224e8e63653f1140a74f3b5
joaosimsic <jpsimsic@hotmail.com>
2026-06-29 15:55:01 -0300

    feat: undo

--- domains/editor/nvim/config/lua/infra/plugins.lua
+++ domains/editor/nvim/config/lua/infra/plugins.lua
@@ -11,5 +11,28 @@ return {
 	},
 	{
 		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				update_debounce = 100,
+        word_diff = false,
+			})
+		end,
 	},
 }
