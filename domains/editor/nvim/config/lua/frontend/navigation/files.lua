9c2b9db9d437d0f761ba0682a88c5c069e112e17
joaosimsic <jpsimsic@hotmail.com>
2026-06-29 12:28:34 -0300

    fix: files

--- domains/editor/nvim/config/lua/frontend/navigation/files.lua
+++ domains/editor/nvim/config/lua/frontend/navigation/files.lua
@@ -9,15 +9,27 @@ return {
 		local binder = Keybinder.new(nil, "FILES")
 
 		binder:nmap("<leader>fl", function()
-			local mark_pos = vim.api.nvim_get_mark("0", {})
-			local line_num = mark_pos[1]
+			local mark_info = vim.api.nvim_get_mark("0", {})
+			local line_num = mark_info[1]
+			local file_path = mark_info[4]
 
-			if line_num <= 0 then
-				vim.notify("No previous buffer found", vim.log.levels.INFO)
+			if line_num <= 0 or file_path == "" then
+				vim.notify("No previous file found", vim.log.levels.INFO)
 				return
 			end
 
-			vim.cmd("normal! '0")
+			file_path = vim.fn.fnamemodify(file_path, ":p")
+
+			pcall(function()
+				vim.cmd("edit " .. vim.fn.fnameescape(file_path))
+			end)
+
+			local current_buf_name = vim.api.nvim_buf_get_name(0)
+			if vim.fn.fnamemodify(current_buf_name, ":p") == file_path then
+				pcall(function()
+					vim.api.nvim_win_set_cursor(0, { line_num, mark_info[2] })
+				end)
+			end
 		end, { desc = "Last visited file" })
 	end,
 }
