5c447fd0881a52a798e88bfbfa2cf678d2af3252
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-06-30 17:40:18 -0300

    feat:yazi root pwd and buffer pwd

--- domains/editor/nvim/config/lua/frontend/navigation/yazi.lua
+++ domains/editor/nvim/config/lua/frontend/navigation/yazi.lua
@@ -1,6 +1,9 @@
 ---@type Keybinder
 local Keybinder = require("common.Keybinder")
 
+---@type Logger
+local Logger = require("common.Logger")
+
 ---@type Plugin
 return {
 	"mikavilpas/yazi.nvim",
@@ -13,13 +16,33 @@ return {
 		},
 	},
 	init = function()
+		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
 	end,
 	config = function()
 		local binder = Keybinder.new(nil, "YAZI")
+		local logger = Logger.new("YAZI")
 
 		binder:nmap("<C-a>", function()
-			require("yazi").toggle()
-		end, { desc = "Toggle yazi" })
+			local current_file = vim.api.nvim_buf_get_name(0)
+
+			if current_file == "" then
+				logger:warn("Current buffer has no file path")
+				return
+			end
+
+			local ok, dir = pcall(vim.fs.dirname, current_file)
+
+			if not ok then
+				logger:warn("Invalid buffer directory")
+				return
+			end
+
+			require("yazi").yazi(nil, dir)
+		end, { desc = "Toggle yazi in buffer cwd" })
+
+		binder:nmap("<C-d>", function()
+			require("yazi").yazi(nil, vim.fn.getcwd())
+		end, { desc = "Toggle yazi in root cwd" })
 	end,
 }
