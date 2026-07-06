3e8666436c119007e48e4b7fcfd3dafe823fe7e2
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-06 09:40:43 -0300

    fix: inherit lib

--- lib/domains/default.nix
+++ lib/domains/default.nix
@@ -4,7 +4,6 @@ let
   scan = import ./scan.nix { inherit lib domainsPath; };
   activation = import ./activation.nix { inherit lib; };
   module = import ./module.nix {
-    inherit lib;
     inherit (activation) mkDomainActivation;
   };
 in
