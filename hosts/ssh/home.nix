ba4076ee97a4c7be48adf40f2a9f517222d30005
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-06 12:28:08 -0300

    fix: pre built bin

--- hosts/ssh/home.nix
+++ hosts/ssh/home.nix
@@ -2,12 +2,42 @@
   imports = [ ../../common/home.nix ];
 
   nixpkgs.overlays = [
-    (self: super: {
-      opencode = super.opencode.overrideAttrs (old: {
-        preBuild = (old.preBuild or "") + ''
-          export GOAMD64=v1
+    (final: prev: {
+      opencode = with final; stdenvNoCC.mkDerivation {
+        pname = "opencode";
+        version = prev.opencode.version;
+
+        src = fetchurl {
+          url = "https://github.com/anomalyco/opencode/releases/download/v${prev.opencode.version}/opencode-linux-x64-baseline.tar.gz";
+          hash = "sha256-MBwkXdgbqA7ft9bu51V/WP4PUXRUH7iRQLdl5VTrxf0=";
+        };
+
+        nativeBuildInputs = [ installShellFiles makeBinaryWrapper ];
+
+        dontConfigure = true;
+        dontBuild = true;
+
+        installPhase = ''
+          runHook preInstall
+
+          install -Dm755 opencode $out/bin/opencode
+          wrapProgram $out/bin/opencode \
+            --prefix PATH : ${lib.makeBinPath [ ripgrep ]} \
+            --set OPENCODE_DISABLE_AUTOUPDATE true
+
+          runHook postInstall
         '';
-      });
+
+        postInstall = ''
+          installShellCompletion --cmd opencode \
+            --bash <($out/bin/opencode completion) \
+            --zsh <(SHELL=/bin/zsh $out/bin/opencode completion)
+        '';
+
+        meta = prev.opencode.meta // {
+          platforms = [ "x86_64-linux" ];
+        };
+      };
     })
   ];
 
