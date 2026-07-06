f32e3f4e5edc2bf9bae15a4fc1718719d3958b38
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-06 10:34:54 -0300

    feat: nu activation

--- hosts/ssh/home.nix
+++ hosts/ssh/home.nix
@@ -1,3 +1,12 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [ ../../common/home.nix ];
+
+  home.file.".profile" = {
+    text = ''
+      if [ -x "$(command -v nu)" ]; then
+        exec nu -l
+      fi
+    '';
+    force = true;
+  };
 }
