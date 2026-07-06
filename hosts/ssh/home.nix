58716838b47cbbb9330cf1f85a1844dfc49ace7d
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-06 11:12:45 -0300

    fix: opencode support for older cpu instructions

--- hosts/ssh/home.nix
+++ hosts/ssh/home.nix
@@ -1,6 +1,16 @@
 { lib, ... }: {
   imports = [ ../../common/home.nix ];
 
+  nixpkgs.overlays = [
+    (self: super: {
+      opencode = super.opencode.overrideAttrs (old: {
+        preBuild = (old.preBuild or "") + ''
+          export GOAMD64=v1
+        '';
+      });
+    })
+  ];
+
   home.file.".profile" = {
     text = ''
       if [ -x "$(command -v nu)" ]; then
