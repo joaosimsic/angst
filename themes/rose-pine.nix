4406ae18b53e87d305e9008c31d6512dcc9f15cd
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-16 11:42:54 -0300

    feat: rose pine

---
+++ themes/rose-pine.nix
@@ -0,0 +1,28 @@
+{
+  palette = {
+    background = {
+      base = "#26233a";
+      variant = "#1f1d2e";
+    };
+    surface = {
+      base = "#31748f";
+      variant = "#9ccfd8";
+    };
+    foreground = {
+      base = "#f6c177";
+      variant = "#e0def4";
+    };
+    accent = {
+      base = "#eb6f92";
+      variant = "#c4a7e7";
+    };
+    dim = "#ebbcba";
+  };
+
+  ansi = {
+    error = "#d46b7a";
+    warn = "#e6ab6c";
+    info = "#7faab8";
+    success = "#7ba07e";
+  };
+}
