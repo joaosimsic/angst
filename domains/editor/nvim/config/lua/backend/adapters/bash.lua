c888d32bf387b616573e3220615c870afd112fb5
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-07-16 14:50:23 -0300

    fix: sh tooling

--- domains/editor/nvim/config/lua/backend/adapters/bash.lua
+++ domains/editor/nvim/config/lua/backend/adapters/bash.lua
@@ -1,9 +1,11 @@
 ---@type Adapter
 return {
-	filetypes = { "bash" },
+	filetypes = { "bash", "sh" },
 	lsp = "bashls",
 	lsp_cmd = { "bash-language-server", "start" },
 	treesitter = "bash",
+	formatter = "shfmt",
+	linter = "shellcheck",
 	compiler = "bash",
 	compiler_cmd = { "bash", "$FILE" },
 }
