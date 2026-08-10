b622a817044a415e7d75fc9765e2d9c21acab23a
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-10 17:50:41 -0300

    fix: enabled experimental feat

--- domains/agents/opencode/module.nix
+++ domains/agents/opencode/module.nix
@@ -8,5 +8,9 @@
 {
   config = lib.mkIf config.domains.agents.opencode.enable {
     home.packages = [ pkgs.opencode ];
+
+    home.sessionVariables = {
+      OPENCODE_EXPERIMENTAL_LSP_TOOL = "true";
+    };
   };
 }
