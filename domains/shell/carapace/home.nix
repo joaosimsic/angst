592fe55ab30b85c55a3b53272263591637d04bce
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-26 08:36:38 -0300

    fix: carapace cache

--- domains/shell/carapace/home.nix
+++ domains/shell/carapace/home.nix
@@ -9,11 +9,9 @@
   config = lib.mkIf config.domains.shell.carapace.enable {
     home = {
       packages = [ pkgs.carapace ];
-      file.".cache/carapace/init.nu".source = pkgs.runCommand "carapace-init.nu" { } ''
-        ${pkgs.carapace}/bin/carapace _carapace nushell > $out
-      '';
       activation.carapace-init = lib.hm.dag.entryAfter [ "installPackages" ] ''
         $DRY_RUN_CMD mkdir -p "$HOME/.cache/carapace"
+        $DRY_RUN_CMD rm -f "$HOME/.cache/carapace/init.nu"
         $DRY_RUN_CMD $VERBOSE_ECHO "Generating carapace nushell completions..."
         $DRY_RUN_CMD ${pkgs.carapace}/bin/carapace _carapace nushell > "$HOME/.cache/carapace/init.nu"
       '';
