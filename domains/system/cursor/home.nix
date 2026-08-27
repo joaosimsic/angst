eab36604fdf3ecf4dd51f5c558dd1af453772872
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-08-27 14:59:45 -0300

    fix(cursor): make system.cursor independent of gtk/x11
    remove gtk.enable and gtk/x11 sub-enables, keep only generic
    home.pointerCursor (env + .icons fallback) so domain works on
    Wayland/Qt without depending on other domains

--- domains/system/cursor/home.nix
+++ domains/system/cursor/home.nix
@@ -10,14 +10,11 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    gtk.enable = true;
     home.pointerCursor = {
       enable = true;
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
       size = 24;
-      gtk.enable = true;
-      x11.enable = true;
     };
   };
 }
