6536e34b241efe5aac021bfe280eb09b3cb68fbf
joaosimsic <jpsimsic@hotmail.com>
2026-09-01 10:21:36 -0300

    fix: vm nvim

--- domains/editor/nvim/home.nix
+++ domains/editor/nvim/home.nix
@@ -22,6 +22,14 @@ in
         vi = "nvim";
         vim = "nvim";
       };
+
+      activation.seedNvimLazyLock = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.config/nvim"
+        if [ ! -f "${config.home.homeDirectory}/.config/nvim/lazy-lock.json" ]; then
+          $DRY_RUN_CMD cp ${./config/lazy-lock.json} "${config.home.homeDirectory}/.config/nvim/lazy-lock.json"
+          $DRY_RUN_CMD chmod 600 "${config.home.homeDirectory}/.config/nvim/lazy-lock.json"
+        fi
+      '';
     };
   };
 }
