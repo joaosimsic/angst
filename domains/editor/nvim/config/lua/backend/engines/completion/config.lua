40bf6296c2ed0f6bb820b5386a2087212814b3f1
joaosimsic <joao.simsic@4x4brasil.com.br>
2026-09-21 17:18:13 -0300

    feat: q fallback

--- domains/editor/nvim/config/lua/backend/engines/completion/config.lua
+++ domains/editor/nvim/config/lua/backend/engines/completion/config.lua
@@ -29,7 +29,7 @@ function M.setup()
 		keymap = {
 			preset = "none",
 			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-			["Q"] = { "hide" },
+			["Q"] = { "hide", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
 
 			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
